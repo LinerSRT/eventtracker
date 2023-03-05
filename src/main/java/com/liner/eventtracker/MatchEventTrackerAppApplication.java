4acd9f7182f71d7bc99e15651df2c6b59de4830f
Line'R <SeriniTY320@gmail.com>
2023-03-05 19:14:22 +0300

    Initial commit

---
+++ src/main/java/com/liner/eventtracker/MatchEventTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.liner.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchEventTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchEventTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liner/eventtracker/MatchEventTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liner.eventtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchEventTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
